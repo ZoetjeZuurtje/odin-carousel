ba6581094ce61d6f0dcc9782ae260d1cd9536a63
Bram <bramstrik@live.com>
2024-11-28 16:01:29 +0100

    Add functionality

--- src/carousel.js
+++ src/carousel.js
@@ -1,1 +1,32 @@
 import './style.css'
+
+
+let imageIndex = 0;
+const imageContainer = document.querySelector('.images-container');
+const imageTracker = document.querySelector('.image-tracker');
+
+function moveCarousel() {
+  for (let element of imageContainer.children) {
+    element.style.transform = `translateX(-${imageIndex * 100}%)`
+  }
+  
+  for (let element of imageTracker.children) {
+    element.classList.remove('active');
+  }
+  imageTracker.children[imageIndex].classList.add('active');
+}
+
+document.querySelector('.carousel .change-carousel-item.arrow-left').addEventListener('click', (_) => {
+  imageIndex--;
+  if (imageIndex < 0) {
+    imageIndex = imageContainer.children.length - 1;
+  }
+  moveCarousel();
+});
+document.querySelector('.carousel .change-carousel-item.arrow-right').addEventListener('click', (_) => {
+  imageIndex++;
+  if (imageIndex >= imageContainer.children.length) {
+    imageIndex = 0;
+  }
+  moveCarousel();
+});
