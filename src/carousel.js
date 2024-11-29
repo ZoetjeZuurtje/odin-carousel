31cb0d933ee2d1c72ab638ec50a4aa9e64763837
Bram <bramstrik@live.com>
2024-11-29 11:38:44 +0100

    Rewrite to use OOP

--- src/carousel.js
+++ src/carousel.js
@@ -1,38 +1,65 @@
 import './style.css'
 
-let imageIndex = 0
-const imageContainer = document.querySelector('.images-container')
-const imageTracker = document.querySelector('.image-tracker')
+class Carousel {
+  constructor (carouselContainer, autoSlide = false) {
+    this.sliderIndex = 0
+    this.autoSlide = autoSlide
+    this.autoSlideTiming = 5E3
+    this.carouselContainer = carouselContainer
+    this.slideContainer = carouselContainer.querySelector('.images-container')
+    this.imageTracker = carouselContainer.querySelector('.image-tracker')
 
-function moveCarousel () {
-  for (const element of imageContainer.children) {
-    element.style.transform = `translateX(-${imageIndex * 100}%)`
+    carouselContainer.querySelectorAll('.image-tracker > .dot').forEach((element) => element.addEventListener('click', (event) => {
+      const i = event.target.dataset.slideIndex
+      this.goToSlide(i)
+    }))
+
+    carouselContainer.querySelector('.change-carousel-item.arrow-right').addEventListener('click', () => {
+      this.increaseIndex()
+      this.moveCarousel()
+    })
+    carouselContainer.querySelector('.change-carousel-item.arrow-left').addEventListener('click', () => {
+      this.decreaseIndex()
+      this.moveCarousel()
+    })
+
+    setInterval(() => {
+      if (autoSlide) {
+        this.increaseIndex()
+        this.moveCarousel()
+      }
+    }, this.autoSlideTiming)
   }
 
-  for (const element of imageTracker.children) {
-    element.classList.remove('active')
+  increaseIndex () {
+    this.sliderIndex++
+    if (this.sliderIndex >= this.slideContainer.children.length) {
+      this.sliderIndex = 0
+    }
   }
-  imageTracker.children[imageIndex].classList.add('active')
+
+  decreaseIndex () {
+    this.sliderIndex--
+    if (this.sliderIndex < 0) {
+      this.sliderIndex = this.slideContainer.children.length - 1
+    }
+  }
+
+  moveCarousel = () => {
+    for (const element of this.slideContainer.children) {
+      element.style.transform = `translateX(-${this.sliderIndex * 100}%)`
+    }
+
+    for (const element of this.imageTracker.children) {
+      element.classList.remove('active')
+    }
+    this.imageTracker.children[this.sliderIndex].classList.add('active')
+  }
+
+  goToSlide (index) {
+    this.sliderIndex = index
+    this.moveCarousel()
+  }
 }
-
-function skipToSlide (event) {
-  const slideIndex = +event.target.dataset.slideIndex
-  imageIndex = slideIndex
-  moveCarousel()
-}
-
-document.querySelectorAll('.image-tracker > .dot').forEach(element => element.addEventListener('click', skipToSlide))
-document.querySelector('.carousel .change-carousel-item.arrow-left').addEventListener('click', (_) => {
-  imageIndex--
-  if (imageIndex < 0) {
-    imageIndex = imageContainer.children.length - 1
-  }
-  moveCarousel()
-})
-document.querySelector('.carousel .change-carousel-item.arrow-right').addEventListener('click', (_) => {
-  imageIndex++
-  if (imageIndex >= imageContainer.children.length) {
-    imageIndex = 0
-  }
-  moveCarousel()
-})
+const carousel = new Carousel(document.querySelector('.carousel-container'), true)
+carousel.autoSlideTiming = 5E3
